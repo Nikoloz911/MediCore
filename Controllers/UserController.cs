b04eea89743d5e563dfacdba6db23edd004c70c5
Nikoloz911 <nikalobjanidze014@gmail.com>
2025-04-23 21:51:30 +0400

    added all Necessary nuget packages, deleted Unnecessary files, added User class and controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediCore.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+}
---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace MediCore.Models;
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; } // Admin, Doctor, Nurse, Patient
+    public bool Status { get; set; } // active, inactive
+}
