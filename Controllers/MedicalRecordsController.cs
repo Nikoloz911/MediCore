f3b4d03c1ae42fc1a9a6e30a7c27271633223eac
Nikoloz911 <nikalobjanidze014@gmail.com>
2025-05-01 22:09:12 +0400

    Added New Service, Controller, Interface for Medical Records

---
+++ Controllers/MedicalRecordsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MediCore.Services.Interfaces;
+using MediCore.Models;
+namespace MediCore.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MedicalRecordsController : ControllerBase
+{
+    private readonly IMedicalRecords _medicalRecordsService;
+    public MedicalRecordsController(IMedicalRecords medicalRecordsService)
+    {
+        _medicalRecordsService = medicalRecordsService;
+    }
+
+
+
+//    GET /api/medical-records/patient/{patientId
+//} - პაციენტის სამედიცინო ჩანაწერები
+//GET /api/medical-records/{id} -კონკრეტული ჩანაწერის დეტალები
+//POST /api/medical-records - ახალი ჩანაწერის შექმნა (Doctor)
+//PUT /api/medical-records/{id} -ჩანაწერის განახლება(Doctor)
+}
---
+++ Services/Implementations/MedicalRecordsService.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using FluentValidation;
+using MediCore.Services.Interfaces;
+using MediCore.Data;
+using MediCore.Request;
+
+namespace MediCore.Services.Implementations;
+public class MedicalRecordsService : IMedicalRecords
+{
+    private readonly DataContext _context;
+    //private readonly IValidator<> _userValidator;
+    private readonly IMapper _mapper;
+
+    public MedicalRecordsService(DataContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+}
