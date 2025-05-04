30e16a5729445cb991b0883a508e9de94bd81bc7
Nikoloz911 <nikalobjanidze014@gmail.com>
2025-05-04 19:18:48 +0400

    Fixed Issue

--- DTOs/PrescriptionsDTOs/GetPatientPrescriptionsDTO.cs
+++ DTOs/PrescriptionsDTOs/GetPatientPrescriptionsDTO.cs
@@ -1,6 +1,4 @@
 ﻿using MediCore.Enums;
-using MediCore.Models;
-
 namespace MediCore.DTOs.PrescriptionsDTOs;
 public class GetPatientPrescriptionsDTO
 {
@@ -9,15 +7,4 @@ public class GetPatientPrescriptionsDTO
     public DateOnly IssueDate { get; set; }
     public DateOnly ExpiryDate { get; set; }
     public PRESCRIPTION_STATUS Status { get; set; }
-    public PrescriptinosPatientBasicInfo Patient { get; set; }
-}
-public class PrescriptinosPatientBasicInfo
-{
-    public int UserId { get; set; }
-    public DateOnly DateOfBirth { get; set; }
-    public GENDER GENDER { get; set; } 
-    public string PhoneNumber { get; set; }
-    public string InsuranceDetails { get; set; }
-    public string Allergies { get; set; }
-    public BLOOD_TYPE BloodType { get; set; } 
 }
